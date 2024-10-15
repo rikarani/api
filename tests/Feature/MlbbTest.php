d158dad552ee17dcc4656b62c23e0c645a3815b1
rikarani <rikarani@users.noreply.github.com>
2024-10-15 15:10:39 +0000

    Fixes coding style

--- tests/Feature/MlbbTest.php
+++ tests/Feature/MlbbTest.php
@@ -5,17 +5,17 @@ namespace Tests\Feature;
 use App\Facades\Codashop;
 use Illuminate\Testing\Fluent\AssertableJson;
 
-describe("API Response", function () {
-    it("should ok and have exact shape of response", function () {
-        Codashop::shouldReceive("mlbb")->once()->andReturn([
-            "success" => true,
-            "confirmationFields" => [
-                "productName" => "Mobile Legends: Bang Bang",
-                "username" => "Jane Doe",
+describe('API Response', function () {
+    it('should ok and have exact shape of response', function () {
+        Codashop::shouldReceive('mlbb')->once()->andReturn([
+            'success' => true,
+            'confirmationFields' => [
+                'productName' => 'Mobile Legends: Bang Bang',
+                'username' => 'Jane Doe',
             ],
-            "user" => [
-                "userId" => "123456789",
-                "zoneId" => "1234",
+            'user' => [
+                'userId' => '123456789',
+                'zoneId' => '1234',
             ],
         ]);
 
@@ -23,81 +23,81 @@ describe("API Response", function () {
          * @var \Illuminate\Testing\TestResponse $response
          * @var \Tests\TestCase $this
          */
-        $response = $this->getJson("/cek-ign/mlbb?id=123456789&zone=1234");
+        $response = $this->getJson('/cek-ign/mlbb?id=123456789&zone=1234');
 
         $response->assertStatus(200)->assertJson(function (AssertableJson $json) {
             $json->hasAll([
-                "success",
-                "code",
-                "data.game",
-                "data.account.ign",
-                "data.account.id",
-                "data.account.zone",
+                'success',
+                'code',
+                'data.game',
+                'data.account.ign',
+                'data.account.id',
+                'data.account.zone',
             ]);
         });
     });
 
-    it("should 404 and have exact shape of response", function () {
-        Codashop::shouldReceive("mlbb")->once()->andReturn([
-            "success" => false
+    it('should 404 and have exact shape of response', function () {
+        Codashop::shouldReceive('mlbb')->once()->andReturn([
+            'success' => false,
         ]);
 
         /**
          * @var \Illuminate\Testing\TestResponse $response
          * @var \Tests\TestCase $this
          */
-        $response = $this->getJson("/cek-ign/mlbb?id=0&zone=0");
+        $response = $this->getJson('/cek-ign/mlbb?id=0&zone=0');
 
         $response->assertStatus(404)->assertJson(function (AssertableJson $json) {
             $json->hasAll([
-                "success",
-                "code",
-                "error.name",
-                "error.message"
+                'success',
+                'code',
+                'error.name',
+                'error.message',
             ]);
         });
     });
 });
 
-describe("Validation", function () {
-    test("missing id from query", function () {
+describe('Validation', function () {
+    test('missing id from query', function () {
         /**
          * @var \Illuminate\Testing\TestResponse $response
          * @var \Tests\TestCase $this
          */
-        $response = $this->getJson("/cek-ign/mlbb?zone=0");
+        $response = $this->getJson('/cek-ign/mlbb?zone=0');
 
         $response->assertStatus(400)->assertJson(function (AssertableJson $json) {
-            $json->hasAll(["success", "code"])->has("errors", 1, function (AssertableJson $json) {
-                $json->where("path", "id")->where("message", "The id field is required.");
+            $json->hasAll(['success', 'code'])->has('errors', 1, function (AssertableJson $json) {
+                $json->where('path', 'id')->where('message', 'The id field is required.');
             });
         });
     });
 
-    test("missing zone from query", function () {
+    test('missing zone from query', function () {
         /**
          * @var \Illuminate\Testing\TestResponse $response
          * @var \Tests\TestCase $this
          */
-        $response = $this->getJson("/cek-ign/mlbb?id=0");
+        $response = $this->getJson('/cek-ign/mlbb?id=0');
 
         $response->assertStatus(400)->assertJson(function (AssertableJson $json) {
-            $json->hasAll(["success", "code"])->has("errors", 1, function (AssertableJson $json) {
-                $json->where("path", "zone")->where("message", "The zone field is required.");
+            $json->hasAll(['success', 'code'])->has('errors', 1, function (AssertableJson $json) {
+                $json->where('path', 'zone')->where('message', 'The zone field is required.');
             });
         });
     });
 
-    test("missing both id and zone from query", function () {
+    test('missing both id and zone from query', function () {
         /**
          * @var \Illuminate\Testing\TestResponse $response
          * @var \Tests\TestCase $this
          */
-        $response = $this->getJson("/cek-ign/mlbb");
+        $response = $this->getJson('/cek-ign/mlbb');
 
         $response->assertStatus(400)->assertJson(function (AssertableJson $json) {
-            $json->hasAll(["success", "code"])->has("errors", 2, function (AssertableJson $json) {
-                $json->hasAll(["path", "message"]);
+            $json->hasAll(['success', 'code'])->has('errors', 2, function (AssertableJson $json) {
+                $json->hasAll(['path', 'message']);
             });
         });
     });
