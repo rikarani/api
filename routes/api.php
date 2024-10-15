9dabcb49df694944b823f392c562b21fe4257a4e
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-15 22:22:46 +0700

    placeholder

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,10 @@ use App\Http\Controllers\CheckController;
 
 Route::prefix('/cek-ign')->group(function () {
     Route::get('/mlbb', [CheckController::class, 'mlbb']);
+    Route::get("/genshin", function () {
+        return response()->json([
+            "status" => "success",
+            "message" => "Genshin Impact is working"
+        ]);
+    });
 });
