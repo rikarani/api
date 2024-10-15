4a24413b91ae28721ea61314a424ad6cdaab6d67
rikarani <rikarani@users.noreply.github.com>
2024-10-15 15:24:20 +0000

    Fixes coding style

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\CheckController;
 
 Route::prefix('/cek-ign')->group(function () {
     Route::get('/mlbb', [CheckController::class, 'mlbb']);
-    Route::get("/genshin", function () {
+    Route::get('/genshin', function () {
         return response()->json([
-            "status" => "success",
-            "message" => "Genshin Impact is working"
+            'status' => 'success',
+            'message' => 'Genshin Impact is working',
         ]);
     });
 });
