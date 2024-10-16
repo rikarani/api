8da55cf31ee677c6bdf996099597c7e52de85163
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-16 16:09:34 +0700

    Refactor CheckController to add Genshin Impact endpoint

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Facades\Check;
+use App\Enums\Genshin;
+use App\Helpers\Server;
 use App\Traits\HasResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class CheckController extends Controller
@@ -21,6 +24,34 @@ class CheckController extends Controller
             ]);
 
             return Check::mlbb($validated['id'], $validated['zone']);
+        } catch (ValidationException $e) {
+            $errors = Collection::make($e->errors())->map(function ($error, $key) {
+                return [
+                    'path' => $key,
+                    'message' => $error[0],
+                ];
+            });
+
+            return $this->badRequest($errors->values());
+        }
+    }
+
+    public function genshin(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'uid' => "required|numeric",
+            ]);
+
+            $server = Server::genshin($validated['uid']);
+
+            if (!$server) {
+                throw ValidationException::withMessages([
+                    'uid' => "Invalid UID"
+                ]);
+            }
+
+            return Check::genshin($validated['uid'], $server->value);
         } catch (ValidationException $e) {
             $errors = Collection::make($e->errors())->map(function ($error, $key) {
                 return [
