b588a2cc7b41c93d110033375f27a33eca8bdf2a
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-16 16:09:58 +0700

    Refactor CheckController to add Genshin Impact endpoint and implement genshin method in Game contract

--- app/Services/Check.php
+++ app/Services/Check.php
@@ -2,9 +2,14 @@
 
 namespace App\Services;
 
+use App\Enums\Genshin;
 use App\Contracts\Game;
+use App\Helpers\Server;
 use App\Facades\Codashop;
 use App\Traits\HasResponse;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Response;
 
 class Check implements Game
 {
@@ -25,6 +30,26 @@ class Check implements Game
                 'id' => $response['user']['userId'],
                 'zone' => $response['user']['zoneId'],
             ],
+        ];
+
+        return $this->ok(collect($data));
+    }
+
+    public function genshin(string $uid, string $server)
+    {
+        $response = Codashop::genshin($uid, $server);
+
+        if ($response["errorCode"] == "-100") {
+            return $this->notFound();
+        }
+
+        $data = [
+            "game" => $response["confirmationFields"]["productName"],
+            "account" => [
+                "ign" => $response["confirmationFields"]["username"],
+                "uid" => $response["user"]["userId"],
+                "server" => Genshin::tryFrom($response["user"]["zoneId"])->name
+            ]
         ];
 
         return $this->ok(collect($data));
